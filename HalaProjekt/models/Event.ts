f669f57e39e6a7ffa00f75a4284780d6c6f85cd6
k-wos <80058894+k-wos@users.noreply.github.com>
2022-05-16 13:41:13 +0200

    a

---
+++ HalaProjekt/models/Event.ts
@@ -0,0 +1,9 @@
+export class Event{
+    name: string
+    artist?: string
+    numberOfPeople : number
+    date: Date
+    ticket: string
+    status: string //nadchodzące/odbyte
+    
+}
---
+++ HalaProjekt/models/Ticket.ts
@@ -0,0 +1,12 @@
+export class ticket{
+    id: number
+    price: number
+    sector: string
+    
+}
+
+
+id
+    - cena
+    - sektor
+    - cena
